852c3e2d81990548aa58c6f596ba9b9d65c7354c
Adrian Soelberg <adri0564@stud.kea.dk>
2023-09-19 16:07:13 +0200

    uden js

--- src/main.js
+++ src/main.js
@@ -17,22 +17,22 @@ import "./style.css";
 //   }
 // }
 
-// Hent referencer til billederne
-const imageLeft = document.querySelector(".justify-self-start");
-const imageRight = document.querySelector(".justify-self-end");
+// // Hent referencer til billederne
+// const imageLeft = document.querySelector(".justify-self-start");
+// const imageRight = document.querySelector(".justify-self-end");
 
-// Lyt efter scrollbegivenheder
-window.addEventListener("scroll", () => {
-  // Beregn scrolle-retningen
-  const scrollPosition = window.scrollY;
+// // Lyt efter scrollbegivenheder
+// window.addEventListener("scroll", () => {
+//   // Beregn scrolle-retningen
+//   const scrollPosition = window.scrollY;
 
-  // Udfør animationen uanset scrolle-retningen
-  imageLeft.style.transform = "translateY(-10%)";
-  imageRight.style.transform = "translateY(-10%)";
+//   // Udfør animationen uanset scrolle-retningen
+//   imageLeft.style.transform = "translateY(-10%)";
+//   imageRight.style.transform = "translateY(-10%)";
 
-  // Brug setTimeout til at returnere billederne til startpositionen efter animationen
-  setTimeout(() => {
-    imageLeft.style.transform = "translateY(0)";
-    imageRight.style.transform = "translateY(0)";
-  }, 400); // Ændr varigheden efter behov
-});
+//   // Brug setTimeout til at returnere billederne til startpositionen efter animationen
+//   setTimeout(() => {
+//     imageLeft.style.transform = "translateY(0)";
+//     imageRight.style.transform = "translateY(0)";
+//   }, 400); // Ændr varigheden efter behov
+// });
